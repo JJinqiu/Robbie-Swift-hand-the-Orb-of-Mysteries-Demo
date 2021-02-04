f88bdc726273426067d0283946f851414b163d0b
蒋金秋 <978472595@qq.com>
2021-02-04 12:06:24 +0800

    06: 跳跃代码(长按跳跃加成)
    -修改BoxCollider
    -修改系统重力Edit - Project Setting - Physics2D - Gravity (！重要！）
    -通过bool变量判断按键提高手感
    -跳跃时间差的方法实现

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,25 @@ public class PlayerMovement : MonoBehaviour
     [Header("移动参数")] public float speed = 8f;
     public float crouchSpeedDivisor = 3f;
 
+    [Header("跳跃参数")] public float jumpForce = 6.3f;
+    public float jumpHoldForce = 1.9f;
+    public float jumpHoldDuration = 0.1f;
+    public float crouchJumpBoost = 2.5f;
+
+    private float m_JumpTime;
+
     [Header("状态")] public bool isCrouch;
+    public bool isOnGround;
+    public bool isJump;
+
+    [Header("环境检测")] public LayerMask groundLayer;
 
     private float m_XVelocity;
+
+    // 按键设置
+    private bool m_JumpPressed;
+    private bool m_JumpHeld;
+    private bool m_CrouchHeld;
 
     // 碰撞体尺寸
     private Vector2 m_ColliderStandSize;
@@ -36,26 +52,75 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_JumpPressed = true;
+        }
+
+        m_JumpHeld = Input.GetButton("Jump");
+        m_CrouchHeld = Input.GetButton("Crouch");
     }
 
     private void FixedUpdate()
     {
+        if (!Input.GetButton("Jump"))
+        {
+            m_JumpPressed = false;
+        }
+
+        PhysicsCheck();
         GroundMovement();
+        MidAirMovement();
+    }
+
+    private void PhysicsCheck()
+    {
+        if (m_Collider.IsTouchingLayers(groundLayer))
+            isOnGround = true;
+        else isOnGround = false;
     }
 
     private void GroundMovement()
     {
-        if (Input.GetButton("Crouch"))
+        if (m_CrouchHeld && !isCrouch && isOnGround)
             Crouch();
-        else if (!Input.GetButton("Crouch") && isCrouch)
+        else if (!m_CrouchHeld && isCrouch)
+            StandUp();
+        else if (!isOnGround && isCrouch) 
             StandUp();
 
         m_XVelocity = Input.GetAxis("Horizontal"); // -1f~1f
         if (isCrouch)
             m_XVelocity /= crouchSpeedDivisor;
-        
+
         m_Rb2d.velocity = new Vector2(m_XVelocity * speed, m_Rb2d.velocity.y);
         FlipDirection();
+    }
+
+    private void MidAirMovement()
+    {
+        if (m_JumpPressed && isOnGround && !isJump)
+        {
+            if (isOnGround && isCrouch)
+            {
+                StandUp();
+                m_Rb2d.AddForce(new Vector2(0f, crouchJumpBoost), ForceMode2D.Impulse);
+            }
+            
+            isOnGround = false;
+            isJump = true;
+
+            m_JumpTime = Time.time + jumpHoldDuration;
+
+            m_Rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+        else if (isJump)
+        {
+            if (m_JumpHeld)
+                m_Rb2d.AddForce(new Vector2(0f, jumpHoldForce), ForceMode2D.Impulse);
+            if (m_JumpTime < Time.time)
+                isJump = false;
+        }
     }
 
     private void FlipDirection()
